dbe3598b85bed49f36dc8f692350cdd61b823d9b
oranmoshe <oran.moshe@correnet.com>
2022-08-15 17:10:28 +0300

    heroku node ping

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const cron = require('node-cron');
+var request = require('request');
 
 const app = express();
 
@@ -11,6 +13,14 @@ app.get('/*', function(req,res) {
 
   res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
+
+cron.schedule('*/60 * * * * *', () => {
+  console.log("call") // Print the google web page.
+  request('https://alma-app-server.herokuapp.com/api/syndicators/1', function (error, response, body) {
+    console.log(response.statusCode) // Print status
+  })
+})
+
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
